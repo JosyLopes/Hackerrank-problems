f45538fa60cd3bb516824e74e1ff1276ea6468ad
Josy-Yuuka <yuukatitania@gmail.com>
2023-07-29 11:48:32 -0300

    Java Inheritance II

---
+++ src/javainheritancedois/Adder.java
@@ -0,0 +1,10 @@
+package javainheritancedois;
+
+class Adder extends Arithmetic {
+
+    public String toString() {
+        return "My superclass is: Arithmetic" ;
+    }   
+    
+    
+}
---
+++ src/javainheritancedois/Solution.java
@@ -0,0 +1,13 @@
+package javainheritancedois;
+
+public class Solution {
+    public static void main(String[] args) {
+        
+        Adder obj = new Adder();
+        
+        obj.toString();
+
+       System.out.println(obj.add(10,32) + " " + obj.add(10,3) + " " + obj.add(10,10));
+
+    }
+}
