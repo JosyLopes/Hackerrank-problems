a8ddcb523eb29955ab9bc0a5c3879f0c04aac548
Rafael N Santos <42522990+rnds1@users.noreply.github.com>
2023-08-06 00:15:43 -0300

    Update Animal.java
    Continue programando minha jovem.

--- src/javainheritance/Animal.java
+++ src/javainheritance/Animal.java
@@ -1,6 +1,16 @@
 package javainheritance;
 
 public class Animal {
+        
+        //raca, classe, filo
+    
+
+    void Breath(){
+        System.out.println("I am Alive");
+    }
+    void Eat(){
+        System.out.println("I am Happy");
+    }
     
     void walk(){
         System.out.println("I am walking");
